3513eb8699db63204ad612e3a1747befb72c477c
Daniel Grahn <daniel_grahn@live.se>
2022-11-27 12:05:13 +0100

    jorå

---
+++ app.js
@@ -0,0 +1,14 @@
+const { writeFileSync, readFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            
+        }
+    })
+})
